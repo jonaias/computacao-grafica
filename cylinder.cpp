60d0d02e6fb59570a389903bd20e4dda7ebbf95c
Jonas Dourado <jonas.jonaias@gmail.com>
2011-06-15 00:42:17 -0300

    Added cylinder

---
+++ cylinder.cpp
@@ -0,0 +1,31 @@
+//      ropepiece.cpp
+//      
+//      Copyright 2011 jonas <jonaias@jonaias-MX6453>
+//      
+//      This program is free software; you can redistribute it and/or modify
+//      it under the terms of the GNU General Public License as published by
+//      the Free Software Foundation; either version 2 of the License, or
+//      (at your option) any later version.
+//      
+//      This program is distributed in the hope that it will be useful,
+//      but WITHOUT ANY WARRANTY; without even the implied warranty of
+//      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//      GNU General Public License for more details.
+//      
+//      You should have received a copy of the GNU General Public License
+//      along with this program; if not, write to the Free Software
+//      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+//      MA 02110-1301, USA.
+
+
+#include "cylinder.h"
+
+Cylinder::Cylinder(string name,GLUquadricObj *quadratic, GLfloat radius, GLfloat height):Object(name,quadratic){
+	this->radius = radius;
+	this->height = height;	
+}
+
+void Cylinder::DrawModel(){
+	TRACE("Drawing cylinder:%s, radius:%f, height:%f\n",name.c_str(),radius,height);
+	gluCylinder(quadratic,radius,radius,height,20,1);
+}
