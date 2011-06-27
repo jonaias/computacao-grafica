b02755eba1cc3ce54f60548ef5ded9ed733e8932
Caio <caio@caio-Aspire-4540.(none)>
2011-06-27 13:40:34 -0300

    heightmap skeleton

--- Makefile
+++ Makefile
@@ -6,8 +6,8 @@ CC = g++ -Wall -ansi -lGL -lGLU -lSDL -lSDL_image -g -pg
 all: ponte_do_mal
 
 
-ponte_do_mal: main.o object.o createscene.o cylinder.o plane.o cube.o
-	$(CC) -o ./bin/ponte_do_mal ./build/object.o ./build/createscene.o ./build/cylinder.o ./build/plane.o ./build/cube.o ./build/main.o 
+ponte_do_mal: main.o object.o createscene.o cylinder.o plane.o cube.o heightmap.o
+	$(CC) -o ./bin/ponte_do_mal ./build/object.o ./build/createscene.o ./build/cylinder.o ./build/plane.o ./build/cube.o ./build/heightmap.o ./build/main.o 
 main.o: main.cpp
 	$(CC) -o ./build/main.o -c main.cpp
 object.o: object.cpp object.h
@@ -20,7 +20,8 @@ createscene.o: createscene.cpp createscene.h
 	$(CC) -o ./build/createscene.o -c createscene.cpp
 cube.o: cube.cpp cube.h
 	$(CC) -o ./build/cube.o -c cube.cpp
-
+heightmap.o: heightmap.cpp heightmap.h
+	$(CC) -o ./build/heightmap.o -c heightmap.cpp
 	
 	
 clean:
---
+++ heightmap.cpp
@@ -0,0 +1,85 @@
+//      heightmap.cpp
+//      
+//      Copyright 2011 jonas <jonaias@jonaias-MX6453>
+//      
+//      This program is free software; you can redistribute it and/or modify
+//      it under the terms of the GNU General Public License as published by
+//      the Free Software Foundation; either version 2 of the License, or
+//      (at your option) any later version.
+//      
+//      This program is distributed in the hope that it will be useful,
+//      but WITHOUT ANY WARRANTY; without even the implied warranty of
+//      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//      GNU General Public License for more details.
+//      
+//      You should have received a copy of the GNU General Public License
+//      along with this program; if not, write to the Free Software
+//      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+//      MA 02110-1301, USA.
+
+
+#include "heightmap.h"
+
+Heightmap::Heightmap(string name,GLUquadricObj *quadratic, GLfloat size):Object(name,quadratic){
+	this->LoadScalef(size,size,size);
+}
+
+void Heightmap::DrawModel(){
+	TRACE("Drawing cylinder:%s, length:%f, width:%f\n",name.c_str(),length,width);
+	// djoubert187 _at_ hotmail.com
+
+
+    glBindTexture(GL_TEXTURE_2D,(texture+4));
+	glBegin(GL_QUADS);		
+		// Front Face (note that the texture's corners have to match the quad's corners)
+		glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);	// Bottom Left Of The Texture and Quad
+		glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);	// Bottom Right Of The Texture and Quad
+		glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f,  1.0f);	// Top Right Of The Texture and Quad
+		glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f,  1.0f);	// Top Left Of The Texture and Quad
+    glEnd();
+    
+    glBindTexture(GL_TEXTURE_2D,(texture+5));
+	glBegin(GL_QUADS);	    
+		// Back Face
+		glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);	// Bottom Right Of The Texture and Quad
+		glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);	// Top Right Of The Texture and Quad
+		glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);	// Top Left Of The Texture and Quad
+		glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f, -1.0f);	// Bottom Left Of The Texture and Quad
+	glEnd();
+	
+	glBindTexture(GL_TEXTURE_2D,(texture+0));
+	glBegin(GL_QUADS);
+		// Top Face
+		glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);	// Top Left Of The Texture and Quad
+		glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f,  1.0f,  1.0f);	// Bottom Left Of The Texture and Quad
+		glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f,  1.0f,  1.0f);	// Bottom Right Of The Texture and Quad
+		glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);	// Top Right Of The Texture and Quad
+    glEnd();
+    
+    glBindTexture(GL_TEXTURE_2D,(texture+1));
+	glBegin(GL_QUADS);
+		// Bottom Face       
+		glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, -1.0f, -1.0f);	// Top Right Of The Texture and Quad
+		glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f, -1.0f, -1.0f);	// Top Left Of The Texture and Quad
+		glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);	// Bottom Left Of The Texture and Quad
+		glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);	// Bottom Right Of The Texture and Quad
+    glEnd();
+    
+    glBindTexture(GL_TEXTURE_2D,(texture+3));
+	glBegin(GL_QUADS);
+		// Right face
+		glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f, -1.0f, -1.0f);	// Bottom Right Of The Texture and Quad
+		glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);	// Top Right Of The Texture and Quad
+		glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f,  1.0f,  1.0f);	// Top Left Of The Texture and Quad
+		glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);	// Bottom Left Of The Texture and Quad
+    glEnd();
+    
+    glBindTexture(GL_TEXTURE_2D,(texture+2));
+	glBegin(GL_QUADS);
+		// Left Face
+		glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);	// Bottom Left Of The Texture and Quad
+		glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);	// Bottom Right Of The Texture and Quad
+		glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f,  1.0f,  1.0f);	// Top Right Of The Texture and Quad
+		glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);	// Top Left Of The Texture and Quad
+    glEnd();                                    
+}
